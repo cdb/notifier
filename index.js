265743e3ce56d3eaa0a1eef4a42cb5142e1ff7ca
Cameron <cdb@github.com>
2020-05-15 05:03:36 +0000

    Update to store Since

--- index.js
+++ index.js
@@ -2,28 +2,49 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fetch = require('node-fetch')
 
+const PERSONAL_ACCESS_TOKEN = core.getInput('PERSONAL_ACCESS_TOKEN')
+const octokit = new github.GitHub(PERSONAL_ACCESS_TOKEN)
+
+const SLACK_URL = core.getInput('SLACK_URL')
+
+const SINCE = core.getInput('SINCE')
+
+async function postNotifications() {
+  const notifs = await octokit.activity.listNotifications({since: SINCE, participating: true, per_page: 100})
+  core.info(`Found ${notifs.data.length} notifications since ${SINCE}`)
+
+  notifs.data.filter(({reason}) => {
+    if (['mention', 'assign', ''].includes(reason)) {
+      return true
+    }
+    if (reason == 'review_requested') {
+      return 
+    }
+  }).
+  map(async (item) => {
+    const {subject: {title, url, type: itemType}} = item
+    
+    const res = await octokit.request(`GET ${url}`)
+
+    if (item.reason == 'review_requested') {
+      core.info("body", res.data.requested_reviewers)
+    }
+    return `Title: ${title} URL: ${res.data.html_url}`
+  }).
+  forEach(async body => {
+    body = await body
+    core.info(`Posting: ${body}`)
+    await fetch(SLACK_URL, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({text: body})
+    })
+  })
+}
+
 async function run() {
   try {
-      const PERSONAL_ACCESS_TOKEN = core.getInput('PERSONAL_ACCESS_TOKEN')
-      const SLACK_URL = core.getInput('SLACK_URL')
-      const octokit = new github.GitHub(PERSONAL_ACCESS_TOKEN)
-      const notifs = await octokit.activity.listNotifications({participating: true, per_page: 100})
-
-      notifs.data.filter(({reason}) => {
-        return ['mention', 'assign'].includes(reason)
-      }).
-      map(async ({subject: {title, url}}) => {
-        const res = await octokit.request(`GET ${url}`)
-        return `Title: ${title} URL: ${res.data.html_url}`
-      }).
-      forEach(async body => {
-        body = await body
-        await fetch(SLACK_URL, {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({text: body})
-        })
-      })
+      await postNotifications()
     } catch (error) {
       core.setFailed(error.message)
     }
